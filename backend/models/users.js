3992fc313991459a3623e3406ffe32de86a0caa8
VagelisN <vagelisn23@gmail.com>
2019-09-03 19:53:17 +0300

    δεν κσερς

---
+++ backend/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// npm install --save mongoose-unique-validator
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  // unique doesnt validate without unique-validator
+  username: {type: String, required: true, unique: true},
+  firstname: {type: String , required: true},
+  lastname: {type: String , required: true},
+  email: {type: String, required: true, unique: true},
+  phone: {type: String, required: true },
+  location: {type: [Number] },
+  afm: {type: Number},
+  password: {type: String, required: true}
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User',userSchema);
